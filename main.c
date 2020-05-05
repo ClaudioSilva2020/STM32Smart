e7408aa738aebc9e02ac807dda4dea191c8016fe
Claudio Silva <claudioccdsilva@gmail.com>
2020-05-05 13:10:35 -0300

    Modificado o main.c

--- main.c
+++ main.c
@@ -9,17 +9,18 @@
 void delayMs(int n);
 
 int main(){
-	RCC->AHB1ENR |= 1;
+	RCC->AHB1ENR |= 1; // Unlook the output digital
 	
 	
-	GPIOA->MODER &= ~0x00000C00;
-	GPIOA->MODER |= 0x00000400;
+	GPIOA->MODER &= ~0x00000C00; // Reset all register of GPIOA
+	GPIOA->MODER |= 0b00000000000000000000010100000000; // Enable GPIO 04 and 05 for output, just GPIO 05(0x00000400)
 		
 	while(1){
+		setBit(GPIOA, 4);
 		setBit(GPIOA, 5);
-		delayMs(500);
+		delayMs(200);
 		clrBit(GPIOA, 5);
-		delayMs(100);
+		delayMs(700);
 	}
 	
 }
